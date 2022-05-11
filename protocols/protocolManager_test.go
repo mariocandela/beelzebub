db8476693e5edf8e1879f301d9ddf257a8630d68
Mario <m4r10.php@gmail.com>
2022-05-11 22:58:21 +0200

    Improve protocolManager_test.go

---
+++ protocols/protocolManager_test.go
@@ -0,0 +1,48 @@
+package protocols
+
+import (
+	"beelzebub/parser"
+	"beelzebub/tracer"
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type mockServiceStrategyValid struct {
+}
+
+func (mockServiceStrategy mockServiceStrategyValid) Init(beelzebubServiceConfiguration parser.BeelzebubServiceConfiguration, tr tracer.Tracer) error {
+	return nil
+}
+
+type mockServiceStrategyError struct {
+}
+
+func (mockServiceStrategy mockServiceStrategyError) Init(beelzebubServiceConfiguration parser.BeelzebubServiceConfiguration, tr tracer.Tracer) error {
+	return errors.New("mockError")
+}
+
+func TestInitServiceManager(t *testing.T) {
+	mockTraceStrategy := func(event tracer.Event) {}
+
+	protocolManager := InitProtocolManager(mockTraceStrategy, mockServiceStrategyValid{})
+
+	assert.NotNil(t, protocolManager.strategy)
+	assert.NotNil(t, protocolManager.tracer)
+}
+
+func TestInitServiceSuccess(t *testing.T) {
+	mockTraceStrategy := func(event tracer.Event) {}
+
+	protocolManager := InitProtocolManager(mockTraceStrategy, mockServiceStrategyValid{})
+
+	assert.Nil(t, protocolManager.InitService(parser.BeelzebubServiceConfiguration{}))
+}
+
+func TestInitServiceError(t *testing.T) {
+	mockTraceStrategy := func(event tracer.Event) {}
+
+	protocolManager := InitProtocolManager(mockTraceStrategy, mockServiceStrategyError{})
+
+	assert.NotNil(t, protocolManager.InitService(parser.BeelzebubServiceConfiguration{}))
+}
