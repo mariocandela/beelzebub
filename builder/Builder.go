c80a54d04e9124a13a7e45054adbc5e9c2a3e467
Mario <mario.candela.personal@gmail.com>
2022-10-22 15:35:50 +0200

    coded rabbitmq into Builder.go

--- builder/Builder.go
+++ builder/Builder.go
@@ -4,17 +4,23 @@ import (
 	"beelzebub/parser"
 	"beelzebub/protocols"
 	"beelzebub/tracer"
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
 	log "github.com/sirupsen/logrus"
 	"io"
 	"os"
 )
 
+const RabbitmqQueueName = "event"
+
 type Beelzebub struct {
 	beelzebubCoreConfigurations    parser.BeelzebubCoreConfigurations
 	beelzebubServicesConfiguration []parser.BeelzebubServiceConfiguration
 	traceStrategy                  tracer.Strategy
+	rabbitMQChannel                *amqp.Channel
 }
 
 func (b *Beelzebub) setBeelzebubCoreConfigurations(beelzebubCoreConfigurations parser.BeelzebubCoreConfigurations) {
@@ -25,7 +31,7 @@ func (b *Beelzebub) setTraceStrategy(traceStrategy tracer.Strategy) {
 	b.traceStrategy = traceStrategy
 }
 
-func (b *Beelzebub) setLogger(configurations parser.Logging) {
+func (b *Beelzebub) buildLogger(configurations parser.Logging) {
 	file, err := os.OpenFile(configurations.LogsPath, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
@@ -44,6 +50,33 @@ func (b *Beelzebub) setLogger(configurations parser.Logging) {
 	}
 }
 
+func (b *Beelzebub) buildRabbitMQ(rabbitMQURI string) error {
+	//TODO manage conn.close()
+	conn, err := amqp.Dial(rabbitMQURI)
+	if err != nil {
+		return err
+	}
+
+	b.rabbitMQChannel, err = conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	_, err = b.rabbitMQChannel.QueueDeclare(
+		RabbitmqQueueName,
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (b *Beelzebub) Run() error {
 	// Init Protocol strategies
 	secureShellStrategy := &protocols.SecureShellStrategy{}
@@ -56,6 +89,30 @@ func (b *Beelzebub) Run() error {
 			"status": event.Status,
 			"event":  event,
 		}).Info("New Event")
+
+		if b.rabbitMQChannel != nil {
+			log.Debug("Push Event on queue")
+			eventJSON, err := json.Marshal(event)
+			if err != nil {
+				log.Error(err.Error())
+				return
+			}
+
+			err = b.rabbitMQChannel.PublishWithContext(
+				context.TODO(),
+				"",
+				RabbitmqQueueName,
+				false,
+				false,
+				amqp.Publishing{
+					ContentType: "application/json",
+					Body:        eventJSON,
+				})
+			if err != nil {
+				log.Error(err.Error())
+				return
+			}
+		}
 	}, hypertextTransferProtocolStrategy)
 
 	for _, beelzebubServiceConfiguration := range b.beelzebubServicesConfiguration {
