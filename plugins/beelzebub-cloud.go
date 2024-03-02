140828bc2be3ad05fbe9169b1d7d13586bd9eaa7
Mario <mario.candela.personal@gmail.com>
2024-03-02 15:15:26 +0100

    add beelzebub cloud integration.

---
+++ plugins/beelzebub-cloud.go
@@ -0,0 +1,49 @@
+package plugins
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/go-resty/resty/v2"
+	"github.com/mariocandela/beelzebub/v3/tracer"
+	log "github.com/sirupsen/logrus"
+)
+
+type beelzebubCloud struct {
+	URI       string
+	AuthToken string
+	client    *resty.Client
+}
+
+func InitBeelzebubCloud(uri, authToken string) *beelzebubCloud {
+	return &beelzebubCloud{
+		URI:       uri,
+		AuthToken: authToken,
+		client:    resty.New(),
+	}
+}
+
+func (beelzebubCloud *beelzebubCloud) SendEvent(event tracer.Event) (bool, error) {
+	requestJson, err := json.Marshal(event)
+	if err != nil {
+		return false, err
+	}
+
+	if beelzebubCloud.AuthToken == "" {
+		return false, errors.New("authToken is empty")
+	}
+
+	response, err := beelzebubCloud.client.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(requestJson).
+		SetHeader("Authorization", beelzebubCloud.AuthToken).
+		SetResult(&gptResponse{}).
+		Post(beelzebubCloud.URI)
+
+	log.Debug(response)
+
+	if err != nil {
+		return false, err
+	}
+
+	return response.StatusCode() == 200, nil
+}
